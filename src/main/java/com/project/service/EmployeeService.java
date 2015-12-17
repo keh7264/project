a2b871b23e81b27e896e90019f0ae2256f48dbb2
keh7264 <keh7264@gmail.com>
2015-12-17 14:11:05 +0900

    add EmployeeService

---
+++ src/main/java/com/project/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.project.service;
+
+import java.util.List;
+
+import com.project.domain.EmployeeVO;
+
+public interface EmployeeService {
+	public void regist(EmployeeVO employee) throws Exception;
+
+	public EmployeeVO read(Integer id) throws Exception;
+
+	public void modify(EmployeeVO employee) throws Exception;
+
+	public void remove(Integer id) throws Exception;
+
+	public List<EmployeeVO> listAll() throws Exception;
+
+}
---
+++ src/main/java/com/project/service/EmployeeServiceImpl.java
@@ -0,0 +1,49 @@
+package com.project.service;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import com.project.domain.EmployeeVO;
+import com.project.persistence.EmployeeDAO;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+	@Inject
+	private EmployeeDAO dao;
+	
+	@Override
+	public void regist(EmployeeVO employee) throws Exception {
+		// TODO Auto-generated method stub
+		dao.create(employee);
+	}
+
+	@Override
+	public EmployeeVO read(Integer id) throws Exception {
+		// TODO Auto-generated method stub
+		return dao.read(id);
+	}
+
+	@Override
+	public void modify(EmployeeVO employee) throws Exception {
+		// TODO Auto-generated method stub
+		dao.update(employee);
+
+	}
+
+	@Override
+	public void remove(Integer id) throws Exception {
+		// TODO Auto-generated method stub
+		dao.delete(id);
+	}
+
+	@Override
+	public List<EmployeeVO> listAll() throws Exception {
+		// TODO Auto-generated method stub
+		return dao.listAll();
+	}
+
+}
